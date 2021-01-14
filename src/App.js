76513f44ec12f80e89d35d89a7e65851a7fc8e11
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-01-14 13:41:50 -0600

    Import connect, render cocktails

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render(){
+
+    const cocktails = this.props.cocktails.map((cocktail, index) => {
+      return <li key={index}>{cocktail.name}</li>;
+    })
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ul>{this.props.loading ? <h3>Loading...</h3> : cocktails}</ul>
+        </header>
+      </div>
+    );
+  }
+
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log(state)
+  return {
+    cocktails: state.cocktailReducer.cocktails,
+    loading: state.cocktailReducer.loading
+  }
+}
+
+export default connect(mapStateToProps)(App);
