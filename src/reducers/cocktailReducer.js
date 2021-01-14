80598f6056f163c5d060d6b4ac1dcd0655ef8b2d
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-01-14 13:14:52 -0600

    Set up initial state

---
+++ src/reducers/cocktailReducer.js
@@ -0,0 +1,7 @@
+export default (state = {cocktails: [], loading: false}, action) => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import cocktailReducer from './cocktailReducer';
+
+const rootReducer = combineReducers({
+    cocktailReducer
+})
+
+export default rootReducer;
