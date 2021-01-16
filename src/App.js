dbcb8b692b0efcf206aca176e5e6840779257298
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-01-15 23:50:20 -0600

    Add header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getCocktails } from './actions/cocktails';
+import Header from './components/Header'
+
 
 class App extends Component {
 
@@ -10,15 +12,21 @@ class App extends Component {
   }
 
   render(){
-
     const cocktails = this.props.cocktails.map((cocktail, index) => {
       return <li key={index}>{cocktail.name}</li>;
     })
 
     return (
       <div className="App">
+        <Header></Header>
         <header className="App-header">
-          <ul>{this.props.loading ? <h3>Loading...</h3> : cocktails}</ul>
+          <React.Fragment>
+            
+            <ul>{this.props.loading ? <h3>Loading...</h3> : cocktails}</ul>
+            
+          </React.Fragment>
+          
+          
         </header>
       </div>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import '../App.css';
+import ReorderIcon from '@material-ui/icons/Reorder';
+import SearchIcon from '@material-ui/icons/Search';
+
+export class Header extends Component {
+
+    state = {
+        clicked: false
+    }
+
+    handleClick = () => {
+        this.setState({
+            clicked: !this.state.clicked
+        })
+    }
+
+    render() {
+        return (
+            <div className="Header">
+                { console.log(this.state) }
+                <div className="leftSide">
+                    <button className="button" onClick={this.handleClick}><ReorderIcon className="buttonImport"/></button>
+                    <div className={this.state.clicked ? "dropMenu" : "links"} >
+                        <a href='/home'>HOME</a>
+                        <a href='/create'>ADD A COCKTAIL</a>
+                        <a href='/favorites'>FAVORITES</a>
+                    </div>
+                    
+                </div>
+                <div className="rightSide">
+                    <input type="text" placeholder="Search..."/>
+                    <button className="button"><SearchIcon/></button>
+                </div>            
+            </div>
+        )
+    }
+
+}
+
+export default Header;
